6b72264043ef34fd91508dc13d64237532d5793e
jared <jared_solano@protonmail.com>
2025-10-31 23:32:13 +0000

    add health checks

--- url-shortener-service/build.gradle.kts
+++ url-shortener-service/build.gradle.kts
@@ -52,6 +52,9 @@ dependencies {
 	// MapStruct
 	implementation("org.mapstruct:mapstruct:1.4.2.Final")
 	annotationProcessor("org.mapstruct:mapstruct-processor:1.4.2.Final")
+
+	// Health check for Spring Boot
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 }
 
 tasks.withType<Test> {
